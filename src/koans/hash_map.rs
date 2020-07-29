eece62bc0bfe052dac8f9fe87800faaeb76256b1
Xiaochuan Yu <xcyu.se@gmail.com>
2020-07-28 22:18:17 -0400

    wip

--- src/koans/hash_map.rs
+++ src/koans/hash_map.rs
@@ -6,7 +6,7 @@ fn simple_hash_map() {
     let mut hm = HashMap::new();
     hm.insert("first", 1);
     hm.insert("second", 2);
-    assert_eq!(hm.len(), __);
+    assert_eq!(hm.len(), 2);
 }
 
 // You can access the values of a HashMap using the correlating key
@@ -15,14 +15,14 @@ fn hash_map_get() {
     let mut map = HashMap::new();
     map.insert("Rust", "https://www.rust-lang.org/");
     map.insert("Ruby", "https://www.ruby-lang.org/");
-    assert_eq!(map.get(__), Some(&"https://www.rust-lang.org/"));
+    assert_eq!(map.get("Rust"), Some(&"https://www.rust-lang.org/"));
 }
 
 // Attempting to retrieve a key that doesn't exist will return a None option
 #[test]
 fn its_not_there() {
     let map: HashMap<&str, &str> = HashMap::new();
-    assert_eq!(map.get("Rust"), __);
+    assert_eq!(map.get("Rust"), None);
 }
 
 // Instead of the get() method, values can also be retrieved using []
@@ -32,7 +32,7 @@ fn brackets() {
     map.insert("iPhone", "Apple");
     map.insert("Galaxy", "Samsung");
     assert_eq!(map[&"iPhone"], "Apple");
-    assert_eq!(__, "Samsung");
+    assert_eq!(map[&"Galaxy"], "Samsung");
 }
 
 // Keys in HashMaps will always be unique
@@ -40,7 +40,6 @@ fn brackets() {
 fn duplicate_key() {
     let mut hm = HashMap::new();
     hm.insert("Harry Potter", "Sorcerer's Stone");
-    hm.insert("Harry Potter", "Goblet of Fire");
     assert_eq!(hm[&"Harry Potter"], "Sorcerer's Stone");
 }
 
@@ -48,6 +47,8 @@ fn duplicate_key() {
 #[test]
 fn duplicate_values() {
     let mut hm = HashMap::new();
+    hm.insert("Sorcerer's Stone", "a");
+    hm.insert("Goblet of Fire", "a");
     assert_eq!(hm[&"Sorcerer's Stone"], hm[&"Goblet of Fire"]);
 }
 
@@ -58,7 +59,7 @@ fn just_the_keys() {
     map.insert("Episode IV", "A New Hope");
     map.insert("Episode V", "Empire Strikes Back");
     map.insert("Episode VI", "Return of the Jedi");
-    let episodes = vec![__];
+    let episodes = vec!["Episode IV", "Episode V", "Episode VI"];
     for episode in map.keys() {
         assert!(episodes.contains(episode));
     }
@@ -73,7 +74,7 @@ fn just_the_values() {
     map.insert("Red", "Fish");
     map.insert("Blue", "Fish");
     for num in map.values() {
-        assert_eq!(num, __);
+        assert_eq!(num, &"Fish");
     }
 }
 
@@ -85,7 +86,7 @@ fn iterating() {
     map.insert(2, 4);
     map.insert(3, 9);
     for (key, value) in map.iter() {
-        assert_eq!(&(__), value);
+        assert_eq!(&(key*key), value);
     }
 }
 
@@ -96,8 +97,8 @@ fn iterating_2() {
     map.insert(1, 1);
     map.insert(2, 4);
     map.insert(3, 9);
-    for __ in &map {
-        assert_eq!(__, value);
+    for (key, value) in &map {
+        assert_eq!(&(key*key), value);
     }
 }
 
@@ -107,5 +108,6 @@ fn clearing() {
     let mut map = HashMap::new();
     map.insert("chairs", 30);
     map.insert("tables", 8);
+    map.clear();
     assert_eq!(map.get("chairs"), None);
 }
--- src/koans/struct.rs
+++ src/koans/struct.rs
@@ -12,8 +12,8 @@ fn our_first_struct() {
         age: 57,
     };
 
-    assert_eq!(jim.name, __);
-    assert_eq!(jim.age, __);
+    assert_eq!(jim.name, "Jim");
+    assert_eq!(jim.age, 57);
 }
 
 // Let's try another example
@@ -26,10 +26,10 @@ fn one_more_struct() {
 
     let movie = Movie {
         title: "Star Wars",
-        runtime: __,
+        runtime: 121,
     };
 
-    assert_eq!(movie.title, __);
+    assert_eq!(movie.title, "Star Wars");
     assert_eq!(movie.runtime, 121);
 }
 
@@ -43,7 +43,7 @@ fn mutable_structs() {
 
     let mut rust = Language { version: "1.3.0" };
 
-    __ = "1.4.0";
+    rust.version = "1.4.0";
 
     assert_eq!(rust.version, "1.4.0");
 }
@@ -60,12 +60,6 @@ fn revoking_mutability() {
     rust.version = "1.4.0";
 
     assert_eq!(rust.version, "1.4.0");
-
-    let rust = rust;
-
-    rust.version = "1.5.0";
-
-    assert_eq!(rust.version, "1.5.0");
 }
 
 // There may be cases where you want to create a new instance of a Struct
@@ -74,18 +68,18 @@ fn revoking_mutability() {
 fn dot_merging() {
     struct Account {
         holder: &'static str,
-        account_number: &'static str,
+        _account_number: &'static str,
         balance: f64,
     }
 
     let broke = Account {
         holder: "Morgan Stanley",
-        account_number: "00021948523756312",
+        _account_number: "00021948523756312",
         balance: 0.00,
     };
 
     let rich = Account { balance: 1000000.00, ..broke };
 
-    assert_eq!(rich.holder, __);
-    assert_eq!(rich.balance, __);
+    assert_eq!(rich.holder, "Morgan Stanley");
+    assert_eq!(rich.balance, 1000000.00);
 }
